776b748dc3e4aa9901610e7da39e9af359caf112
Mario Alvarez <mario.alvarez.san@gmail.com>
2022-04-05 15:20:11 -0400

    Added ability to create multiple lists. Individual list functionality lost.

--- script.js
+++ script.js
@@ -1,54 +1,60 @@
 var button = document.getElementById("add");
-var input = document.getElementById("userinput");
-var list = document.querySelector("ul");
+var listInput = document.getElementById("listinput");
+var listDiv = document.getElementById("listDiv")
 
 
 
-function inputFocus (){
-	input.focus();
+function listInputFocus (){
+	listInput.focus();
 }
+
 function inputLength() {
-	return input.value.length;
+	return listInput.value.length;
 }
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
-		createListElement();
-		inputFocus();
+		createList();
 	}
 }
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
+		createList();
 	}
 }
-function createListElement() {
-	var checkbox = document.createElement("input");
-	checkbox.setAttribute("type", "checkbox");
-	checkbox.setAttribute("id", input.value);
-	checkbox.addEventListener('click', toggleDone);
 
-	var label = document.createElement("label");
-	label.setAttribute("for", input.value);
+// function addItemAfterKeypress(event) {
+// 	if (itemInuputLength > 0 && event.keyCode === 13) {
+// 		return true 
+// 	}
+// }
 
-	var li = document.createElement("li");
-
-	var deleteButton = document.createElement("button");
-	deleteButton.innerHTML = "Delete"
-	deleteButton.addEventListener("click", deleteLi)
-
-	label.appendChild(checkbox);
-	label.append(`${input.value}`);
-	label.appendChild(deleteButton);
-	li.appendChild(label);
-	list.insertBefore(li, list.firstChild);
-	input.value = "";
+function createList() {
+	var itemInput = document.createElement("input");
+	itemInput.setAttribute("type", "text");
+	itemInput.setAttribute("placeholder", "List Item");
+	// itemInput.addEventListener("keypress", addItemAfterKeypress);
+	
+	var list = document.createElement("ul");
+	var deleteButton = createDeleteButton();
+	var listTitle = document.createElement("h2");
+	var div1 = document.createElement("div");
+	var div2 = document.createElement("div");
+	
+	listTitle.innerHTML = `${listInput.value}`;
+	listTitle.appendChild(deleteButton);
+	listTitle.appendChild(div2);
+	div2.appendChild(itemInput);
+	div2.appendChild(list);
+	div1.appendChild(listTitle);
+	listDiv.appendChild(div1);
+	listInput.value = "";
 }
 
 function deleteLi(){
 	this.parentNode.parentNode.remove();
-	inputFocus();
+	listInputFocus();
 }
 
 function toggleDone(item) {
@@ -60,13 +66,52 @@ function toggleDone(item) {
 	} else {
 		list.insertBefore(listItem, list.firstChild)
 	}
-	inputFocus();
 }
 
+
+function createDeleteButton () {
+	var deleteButton = document.createElement("button");
+	deleteButton.innerHTML = "Delete"
+	deleteButton.addEventListener("click", deleteLi)
+	return deleteButton;
+}
 button.addEventListener("click", addListAfterClick);
 
-input.addEventListener("keypress", addListAfterKeypress);
+listInput.addEventListener("keypress", addListAfterKeypress);
 
-document.addEventListener("click", inputFocus);
+listInputFocus();
 
-inputFocus();
+// function createItemInput() {
+// 	var itemInput = document.createElement("input");
+// 	itemInput.setAttribute("type", "text");
+// 	itemInput.setAttribute("placeholder", "List Item");
+// 	var itemInuputLength = itemInput.value.length;
+// 	var itemTrue = itemInput.addEventListener("keypress", addItemAfterKeypress);
+// 	if (itemTrue == true) {
+// 		createlistItem
+// 	}
+// }
+
+
+// function createlistItem(itemValue) {
+// 	var checkbox = document.createElement("input");
+// 	checkbox.setAttribute("type", "checkbox");
+// 	checkbox.setAttribute("id", itemInput.value);
+// 	checkbox.addEventListener('click', toggleDone);
+
+// 	var label = document.createElement("label");
+// 	label.setAttribute("for", itemInput.value);
+
+// 	var li = document.createElement("li");
+
+// 	var deleteButton = document.createElement("button");
+// 	deleteButton.innerHTML = "Delete";
+// 	deleteButton.addEventListener("click", deleteLi);
+
+// 	label.appendChild(checkbox);
+// 	label.append(`${input.value}`);
+// 	label.appendChild(deleteButton);
+// 	li.appendChild(label);
+// 	list.insertBefore(li, list.firstChild);
+// 	itemInput.value = "";
+// }
